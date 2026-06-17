cea96510d5249affaa50491575182d3c052e7938
XLR8discovery <34426105+XLR8discovery@users.noreply.github.com>
2026-06-17 06:23:11 -0400

    Add files via upload

---
+++ scale_test.go
@@ -0,0 +1,23 @@
+// Copyright (C) 2026 XLR8discovery PBC
+// See LICENSE for copying information.
+
+package container
+
+import (
+	"testing"
+
+	"github.com/compose-spec/compose-go/v2/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestScale(t *testing.T) {
+	k := types.ServiceConfig{
+		Name:  "storagenode",
+		Image: "foobar",
+	}
+
+	err := scale(&k, "10")
+	require.NoError(t, err)
+
+	require.Equal(t, 10, *k.Deploy.Replicas)
+}
