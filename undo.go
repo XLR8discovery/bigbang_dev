5cd62e87b7c5c9d09cdf67c02d7cf0a0297de73d
XLR8discovery <34426105+XLR8discovery@users.noreply.github.com>
2026-06-22 11:37:19 -0400

    Add files via upload

---
+++ undo.go
@@ -0,0 +1,45 @@
+// Copyright (C) 2026 XLR8discovery PBC
+// See LICENSE for copying information.
+
+package history
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"xlr8d.io/bigbang-up/cmd"
+	"xlr8d.io/bigbang-up/pkg/common"
+)
+
+func undoCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "undo",
+		Short: "revert to a previous version of the generated docker compose file",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			newTemplateBytes, err := common.Store.RestoreLatestVersion()
+			if err != nil {
+				return err
+			}
+			if newTemplateBytes == nil {
+				return fmt.Errorf("no previous version of the compose file found")
+			}
+			newTemplate, err := common.LoadComposeFromBytes(newTemplateBytes)
+			if err != nil {
+				return err
+			}
+			pwd, _ := os.Getwd()
+			err = common.WriteComposeFileNoHistory(pwd, newTemplate)
+			if err != nil {
+				return err
+			}
+			return nil
+		},
+	}
+}
+
+func init() {
+	cmd.RootCmd.AddCommand(undoCmd())
+}
