198db516056c074f8d7324ff1485168866cb8f96
XLR8discovery <34426105+XLR8discovery@users.noreply.github.com>
2026-07-02 07:09:51 -0400

    Add files via upload

---
+++ persist_test.go
@@ -0,0 +1,37 @@
+// Copyright (C) 2026 XLR8discovery PBC
+// See LICENSE for copying information.
+
+package modify
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"xlr8d.io/bigbang-up/cmd/testdata"
+	"xlr8d.io/bigbang-up/pkg/runtime/runtime"
+)
+
+func TestPersistCockroach(t *testing.T) {
+	dir := t.TempDir()
+
+	st, rt, err := testdata.InitCompose(dir)
+	require.NoError(t, err)
+
+	err = runtime.ApplyRecipes(st, rt, []string{"cockroach"}, 0)
+	require.NoError(t, err)
+
+	err = persist(st, rt, []string{"cockroach"})
+	require.NoError(t, err)
+
+	require.NoError(t, rt.Write())
+
+	result, err := os.ReadFile(filepath.Join(dir, "docker-compose.yaml"))
+	require.NoError(t, err)
+
+	require.Contains(t, string(result), "source: "+filepath.Join(dir, "/cockroach/cockroach-data"))
+	require.Contains(t, string(result), "target: /cockroach/cockroach-data")
+
+}
