5f61ced5298c1f7bef9ec641c49a1d3a55075f24
XLR8discovery <34426105+XLR8discovery@users.noreply.github.com>
2026-07-07 07:19:22 -0400

    Add files via upload

---
+++ port.go
@@ -0,0 +1,82 @@
+// Copyright (C) 2026 XLR8discovery PBC
+// See LICENSE for copying information.
+
+package modify
+
+import (
+	"strconv"
+
+	"github.com/spf13/cobra"
+
+	"xlr8d.io/bigbang-up/cmd"
+	"xlr8d.io/bigbang-up/pkg/common"
+	"xlr8d.io/bigbang-up/pkg/recipe"
+	"xlr8d.io/bigbang-up/pkg/runtime/runtime"
+)
+
+var overrideExternalPort int
+
+func addPortCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "add <selector>... <port>",
+		Aliases: []string{"addport"},
+		Short:   "Add port forward to a container",
+		Long:    cmd.SelectorHelp,
+		Args:    cobra.MinimumNArgs(2),
+		RunE:    cmd.ExecuteBigbangUP(addPort),
+	}
+	cmd.Flags().IntVarP(&overrideExternalPort, "external", "e", 0, "specify a different external port to forward than the internal port (default: internal port)")
+	return cmd
+}
+
+func removePortCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:     "remove <selector>... <port>",
+		Aliases: []string{"removeport", "rm", "delete"},
+		Short:   "remove port forward from a container",
+		Long:    "Remove port forward with the given target port from selected containers. " + cmd.SelectorHelp,
+		Args:    cobra.MinimumNArgs(2),
+		RunE:    cmd.ExecuteBigbangUP(removePort),
+	}
+}
+
+func init() {
+	portCmd := cobra.Command{
+		Use:   "port",
+		Short: "add/remove port forward in specified services",
+	}
+	portCmd.AddCommand(addPortCmd())
+	portCmd.AddCommand(removePortCmd())
+	cmd.RootCmd.AddCommand(&portCmd)
+}
+
+func addPort(st recipe.Stack, rt runtime.Runtime, args []string) error {
+	selector, port := common.SplitArgsSelector1(args)
+	return runtime.ModifyService(st, rt, selector, func(s runtime.Service) error {
+		internal, err := strconv.Atoi(port)
+		if err != nil {
+			return err
+		}
+		if overrideExternalPort == 0 {
+			overrideExternalPort = internal
+		}
+		return s.AddPortForward(runtime.PortMap{
+			Internal: internal,
+			External: overrideExternalPort,
+			Protocol: "tcp",
+		})
+	})
+}
+
+func removePort(st recipe.Stack, rt runtime.Runtime, args []string) error {
+	selector, port := common.SplitArgsSelector1(args)
+	return runtime.ModifyService(st, rt, selector, func(s runtime.Service) error {
+		port, err := strconv.Atoi(port)
+		if err != nil {
+			return err
+		}
+		return s.RemovePortForward(runtime.PortMap{
+			Internal: port,
+		})
+	})
+}
