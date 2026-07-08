3f9ee4e7977252b45ed80fbc482b304fc87db02e
XLR8discovery <34426105+XLR8discovery@users.noreply.github.com>
2026-07-08 06:57:28 -0400

    Add files via upload

---
+++ version.go
@@ -0,0 +1,41 @@
+// Copyright (C) 2026 XLR8discovery PBC
+// See LICENSE for copying information.
+
+package modify
+
+import (
+	"strings"
+
+	"github.com/compose-spec/compose-go/v2/types"
+	"github.com/spf13/cobra"
+
+	"xlr8d.io/bigbang-up/cmd"
+	"xlr8d.io/bigbang-up/pkg/common"
+	"xlr8d.io/bigbang-up/pkg/recipe"
+	"xlr8d.io/bigbang-up/pkg/runtime/runtime"
+)
+
+func versionCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "version <selector>... <version>",
+		Short: "set version (docker image tag) for specified services",
+		Args:  cobra.MinimumNArgs(2),
+		RunE: cmd.ExecuteBigbangUP(func(st recipe.Stack, rt runtime.Runtime, args []string) error {
+			selector, version := common.SplitArgsSelector1(args)
+			return cmd.ChangeCompose(st, rt, selector, func(composeService *types.ServiceConfig) error {
+				return updateVersion(composeService, version)
+			})
+		}),
+	}
+}
+
+func init() {
+	cmd.RootCmd.AddCommand(versionCmd())
+}
+
+func updateVersion(composeService *types.ServiceConfig, version string) error {
+	newImage := strings.ReplaceAll(composeService.Image, "@sha256", "")
+	newImage = strings.Split(newImage, ":")[0] + ":" + version
+	composeService.Image = newImage
+	return nil
+}
