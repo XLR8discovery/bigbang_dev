950b4799bddc7f228be527b4437a15971b137a97
XLR8discovery <34426105+XLR8discovery@users.noreply.github.com>
2026-07-21 07:43:05 -0400

    Rename help.go to cmd/help.go

--- help.go
+++
@@ -1,7 +0,0 @@
-// Copyright (C) 2026 XLR8discovery PBC
-// See LICENSE for copying information.
-
-package cmd
-
-// SelectorHelp is a general note about how selectors supposed to be used.
-const SelectorHelp = "<selector> is a service name (like storagenode) maybe with the index (storagenode1) or a recipe (like minimal or db). Recipes can be printed with `bigbang-up recipes`"
