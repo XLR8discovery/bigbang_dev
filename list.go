b26b16f7c4ff81e7c271ab9f47003cb2be7f246e
XLR8discovery <34426105+XLR8discovery@users.noreply.github.com>
2026-07-24 09:25:07 -0400

    Add files via upload

---
+++ list.go
@@ -0,0 +1,51 @@
+// Copyright (C) 2026 XLR8discovery PBC
+// See LICENSE for copying information.
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/zeebo/errs/v2"
+
+	"xlr8d.io/bgbang-up/pkg/recipe"
+)
+
+func listCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "list",
+		Args:  cobra.NoArgs,
+		Short: "print all the configured services with user versions",
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			pwd, err := os.Getwd()
+			if err != nil {
+				return errs.Wrap(err)
+			}
+			runtime, err := FromDir(pwd)
+			if err != nil {
+				return errs.Wrap(err)
+			}
+			st, err := recipe.GetStack()
+			if err != nil {
+				return errs.Wrap(err)
+			}
+
+			err = runtime.Reload(st)
+			if err != nil {
+				return errs.Wrap(err)
+			}
+			services := runtime.GetServices()
+			for _, s := range services {
+				fmt.Printf("%s\n", s.ID())
+			}
+
+			return nil
+		},
+	}
+}
+
+func init() {
+	RootCmd.AddCommand(listCmd())
+}
