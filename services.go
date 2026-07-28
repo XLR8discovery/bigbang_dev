8a997ea0b792ecf2ea8060449650c3a448b60e9f
XLR8discovery <34426105+XLR8discovery@users.noreply.github.com>
2026-07-28 12:46:07 -0400

    Add files via upload

---
+++ services.go
@@ -0,0 +1,44 @@
+// Copyright (C) 2026 XLR8discovery PBC
+// See LICENSE for copying information.
+
+package cmd
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+
+	"xlr8d.io/bigbang-up/pkg/recipe"
+)
+
+func serviceCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:     "recipes",
+		Aliases: []string{"recipe", "services"},
+		Short:   "Return available recipes and included service names",
+		Args:    cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			fmt.Println("The following predefined recipes are available:")
+			fmt.Println()
+			stack, err := recipe.GetStack()
+			if err != nil {
+				return err
+			}
+			for _, r := range stack {
+				var services []string
+				for _, s := range r.Add {
+					services = append(services, s.Name)
+				}
+				fmt.Printf("%-10s %s (%s)\n", r.Name, r.Description, strings.Join(services, ","))
+			}
+			fmt.Println("")
+			fmt.Println("You can use both the service names (like satellite-api) or recipe names (like minimal) when you init/add clusters")
+			return nil
+		},
+	}
+}
+
+func init() {
+	RootCmd.AddCommand(serviceCmd())
+}
