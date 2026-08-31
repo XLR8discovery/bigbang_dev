feaabb5f7772973eba0e62c7e029e080a1df2c81
XLR8discovery <34426105+XLR8discovery@users.noreply.github.com>
2026-08-31 12:04:25 -0400

    Add files via upload

---
+++ args.go
@@ -0,0 +1,30 @@
+// Copyright (C) 2026 XLR8discovery PBC
+// See LICENSE for copying information.
+
+package common
+
+import (
+	"fmt"
+	"strings"
+)
+
+// ParseArgumentsWithSelector separate the selector and real arguments from os args.
+func ParseArgumentsWithSelector(args []string, argNo int) ([]string, []string, error) {
+	if argNo > len(args) {
+		return nil, nil, fmt.Errorf("not enough arguments (required <selector> + %d)", argNo)
+	}
+	realArgs := args[len(args)-argNo:]
+
+	selectors := make([]string, 0)
+	for i := 0; i < len(args)-argNo; i++ {
+		selectors = append(selectors, strings.Split(args[i], ",")...)
+	}
+
+	return selectors, realArgs, nil
+}
+
+// SplitArgsSelector1 splits selector from arguments leaving one extra separate argument.
+// This is useful for splitting "[<selector>...] arg" or "<selector>... arg".
+func SplitArgsSelector1(args []string) (selector []string, arg string) {
+	return args[:len(args)-1], args[len(args)-1]
+}
