4764b579c899ad3eb0921aee87b3da132705b45a
XLR8discovery <34426105+XLR8discovery@users.noreply.github.com>
2026-09-01 08:14:15 -0400

    Add files via upload

---
+++ compose_test.go
@@ -0,0 +1,19 @@
+// Copyright (C) 2026 XLR8discovery PBC
+// See LICENSE for copying information.
+
+package common
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_ResolveService(t *testing.T) {
+	services, err := ResolveServices([]string{"minimal", "db"})
+	require.NoError(t, err)
+	expected := []string{"spanner", "redis", "satellite-api", "storagenode"}
+	require.ElementsMatch(t,
+		expected,
+		services)
+}
