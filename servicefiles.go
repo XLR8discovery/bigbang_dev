869541b0782837954733067a5394e2020f287f9a
XLR8discovery <34426105+XLR8discovery@users.noreply.github.com>
2026-09-09 07:36:08 -0400

    Add files via upload

---
+++ servicefiles.go
@@ -0,0 +1,22 @@
+// Copyright (C) 2026 XLR8discovery PBC
+// See LICENSE for copying information.
+
+package common
+
+import (
+	"os"
+	"path/filepath"
+)
+
+// ExtractFile extract embedded file, if doesn't exist.
+func ExtractFile(path, fileName string, content []byte) error {
+	newpath := filepath.Join(".", path)
+	err := os.MkdirAll(newpath, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	if _, err := os.Stat(newpath + "/" + fileName); os.IsNotExist(err) {
+		return os.WriteFile(newpath+"/"+fileName, content, 0o644)
+	}
+	return nil
+}
